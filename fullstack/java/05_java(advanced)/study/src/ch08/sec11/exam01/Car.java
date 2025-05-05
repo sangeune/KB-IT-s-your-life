bdacbea8e3089e3019a5138ed6a4dafc8019f351
sangeune <sangeun504@naver.com>
2025-05-05 17:03:43 +0900

    05_java(advanced) 완료

--- fullstack/java/05_java(advanced)/study/src/ch08/sec11/exam01/Car.java
+++ fullstack/java/05_java(advanced)/study/src/ch08/sec11/exam01/Car.java
@@ -2,7 +2,7 @@ package ch08.sec11.exam01;
 
 public class Car {
     Tire tire1 = new HankookTire();
-    Tire tire2 = new KumhoTire();
+    Tire tire2 = new HankookTire();
 
     void run() {
         tire1.roll();
--- fullstack/java/05_java(advanced)/study/src/ch08/sec11/exam01/CarExample.java
+++ fullstack/java/05_java(advanced)/study/src/ch08/sec11/exam01/CarExample.java
@@ -4,5 +4,11 @@ public class CarExample {
     public static void main(String[] args) {
         Car myCar = new Car();
         myCar.run();
+
+        //타이어 객체 교체
+        myCar.tire1 = new KumhoTire();
+        myCar.tire2 = new KumhoTire();
+
+        myCar.run();
     }
 }
