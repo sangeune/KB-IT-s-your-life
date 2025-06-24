6b8f8d4ccfcc1055223a7ebeb9dd8535fc427b97
sangeune <sangeun504@naver.com>
2025-06-24 16:34:52 +0900

    10_spring(basic) 추가

---
+++ fullstack/spring/04_spring(advanced)/board/src/main/java/org/scoula/board/service/BoardServiceImpl.java
@@ -0,0 +1,59 @@
+package org.scoula.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.board.dto.BoardDTO;
+import org.scoula.board.mapper.BoardMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    final private BoardMapper mapper;
+
+    @Override
+    public List<BoardDTO> getList() {
+        log.info("getList......");
+
+        return mapper.getList().stream()
+                .map(BoardDTO::of)
+                .toList();
+    }
+
+    @Override
+    public BoardDTO get(Long no) {
+        log.info("get......" + no);
+
+        BoardDTO board = BoardDTO.of(mapper.get(no));
+        return Optional.ofNullable(board)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public void create(BoardDTO board) {
+        log.info("create......" + board);
+
+        mapper.create(board.toVO());
+    }
+
+    @Override
+    public boolean update(BoardDTO board) {
+        log.info("update......" + board);
+
+        return mapper.update(board.toVO()) == 1;
+    }
+
+    @Override
+    public boolean delete(Long no) {
+        log.info("delete......." + no);
+
+        return mapper.delete(no) == 1;
+    }
+}
+
+
--- fullstack/spring/README.md
+++ fullstack/spring/README.md
@@ -13,5 +13,5 @@
 | 07   | OpenAPI                         |  ✅  |  ❎  |
 | 08   | Spring AOP                      |  ✅  |      |
 | 09   | Spring Security Form 인증       |  ✅  |      |
-| 10   | Spring Security JWT 인증        |      |      |
+| 10   | Spring Security JWT 인증        |  ✅  |      |
 | 11   | Spring Security JWT 인증2       |      |      |
